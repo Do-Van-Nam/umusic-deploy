002ce7fc84826d5f6c62e1cb473ae1fdfe38f09e
nam <nam15sep2003@gmail.com>
2025-04-22 17:03:58 -0700

    2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3843307808"
+    serviceWorkerVersion: "610756331"
   }
 });
